2c72687a977549eebc9ada4b115cc1f394d1d681
KodoDushimimana <72461740+KodoDushimimana@users.noreply.github.com>
2022-03-02 18:04:21 +0200

    I have added a schedule to run when the date is yesterday

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-import express from 'express'
-import Airtable from 'airtable'
-import fetch from 'node-fetch'
-import dayjs from 'dayjs'
-import 'dotenv/config'
+const express= require('express') 
+const Airtable = require('airtable') 
+const fetch = require('node-fetch') 
+const dayjs = require('dayjs') 
+const schedule = require('node-schedule')
+
+
+require('dotenv').config()
+
+
+
+
 
 const app = express()
 app.use(express.static('public'))
@@ -13,7 +20,7 @@ const airtable_api = process.env.AIRTABLE_KEY
 const base = new Airtable({apiKey: `${airtable_api}`}).base('appkAFcXYmg8XNqMD');
 const day = '2022-02-18'
 const yesterday = dayjs().add(-1, 'day').format("YYYY-MM-DD", "America/New_York")
-console.log(yesterday)
+
 
 const xom_data = `https://api.polygon.io/v1/open-close/XOM/${yesterday}?adjusted=true&apiKey=${stockMarket}`
 const rio_data = `https://api.polygon.io/v1/open-close/RIO/${yesterday}?adjusted=true&apiKey=${stockMarket}`
@@ -28,9 +35,10 @@ app.get('/xomapi', async(req, res) =>{
     const symbolx = xomData.symbol
     const closingDay = xomData.from
     const closePrice = xomData.close
-    console.log(xomData)
+    
 
-    base('Table 1').update([
+    const updateXom = () =>{
+      base('Table 1').update([
         {
           "id": "recbrh7FtuGgG3tzV",
           "fields": {
@@ -50,6 +58,16 @@ app.get('/xomapi', async(req, res) =>{
           console.log(record.get('Exchange'));
         });
       });
+    }
+
+   
+    schedule.scheduleJob(yesterday, () =>{
+      updateXom()      
+    })
+    
+
+      
+   
       
 
 
@@ -59,12 +77,13 @@ app.get('/rioapi', async(req, res) =>{
     const fetchrio  = await fetch(rio_data)
     const rioData = await fetchrio.json()
     res.send(rioData)
-    console.log(rioData)
+    
     const symbolx = rioData.symbol
     const closingDay = rioData.from
     const closePrice = rioData.close
 
-    base('Table 1').update([
+    const rioUpdate = () =>{
+       base('Table 1').update([
         {
           "id": "recdychKZxJtk1vc2",
           "fields": {
@@ -84,18 +103,27 @@ app.get('/rioapi', async(req, res) =>{
           console.log(record.get('Exchange'));
         });
       });
+    }
+
+    schedule.scheduleJob(yesterday, () =>{
+      rioUpdate()      
+    })
+   
       
 
-})
+})// closing of rio
+
 app.get('/shlxapi', async(req, res) =>{
     const fetchshlx  = await fetch(shlx_data)
     const shlxData = await fetchshlx.json()
     res.send(shlxData)
-    console.log(shlxData)
+    
     const symbolx = shlxData.symbol
     const closingDay = shlxData.from
     const closePrice = shlxData.close
-    base('Table 1').update([
+
+    const shlxUpdate = () =>{
+        base('Table 1').update([
         {
           "id": "recBT5D2B0pfVfVBz",
           "fields": {
@@ -115,19 +143,28 @@ app.get('/shlxapi', async(req, res) =>{
           console.log(record.get('Exchange'));
         });
       });
+    }
+  
+    schedule.scheduleJob(yesterday, () =>{
+      shlxUpdate()      
+    })
+
+}) // closing of shlx
 
 
-})
+
+
 app.get('/pyplxapi', async(req, res) =>{
     const fetchpypl  = await fetch(pypl_data)
     const pyplData = await fetchpypl.json()
     res.send(pyplData)
-    console.log(pyplData)
+    
     const symbolx = pyplData.symbol
     const closingDay = pyplData.from
     const closePrice = pyplData.close
-    console.log(closingDay)
-    base('Table 1').update([
+    
+    const pyplUpdate = () =>{
+      base('Table 1').update([
         {
           "id": "recC9hLJj6Qj8PADw",
           "fields": {
@@ -147,20 +184,25 @@ app.get('/pyplxapi', async(req, res) =>{
           console.log(record.get('Exchange'));
         });
       });
+    }
+    
+    schedule.scheduleJob(yesterday, () =>{
+      pyplUpdate()      
+    })
 
-
-})
+})// closing of pypl
 
 app.get('/rivnxapi', async(req, res) =>{
     const fetchrivn  = await fetch(rivn_data)
     const rivnData = await fetchrivn.json()
     res.send(rivnData)
-    console.log(rivnData)
+   
     const symbolx = rivnData.symbol
     const closingDay = rivnData.from
     const closePrice = rivnData.close
-    console.log(closingDay)
-    base('Table 1').update([
+
+    const updateRivn = () =>{
+      base('Table 1').update([
         {
           "id": "recJpxr77WedTNGB5",
           "fields": {
@@ -180,6 +222,11 @@ app.get('/rivnxapi', async(req, res) =>{
           console.log(record.get('Exchange'));
         });
       });
+    }
+    
+    schedule.scheduleJob(yesterday, () =>{
+      updateRivn()      
+    })
 
 })
 
